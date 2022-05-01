b521298c09f82c8019cbf40b5b9bbed9655aee4e
DianaM411 <dianamunteanu220@gmail.com>
2022-05-01 19:46:16 +0200

    js menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// mobile navigation
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
